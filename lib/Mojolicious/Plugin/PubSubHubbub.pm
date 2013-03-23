8715adb88c5dbfcd8dd19712d1ee495683f9402d
Nils Diewald <akron@cpan.org>
2013-03-23 03:14:15 +0000

    Import of AKRON/Mojolicious-Plugin-PubSubHubbub-0.05 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-PubSubHubbub
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         AKRON/Mojolicious-Plugin-PubSubHubbub-0.05.tar.gz
    gitpan-cpan-author:       AKRON
    gitpan-cpan-maturity:     released

--- lib/Mojolicious/Plugin/PubSubHubbub.pm
+++ lib/Mojolicious/Plugin/PubSubHubbub.pm
@@ -5,7 +5,7 @@ use Mojo::DOM;
 use Mojo::ByteStream 'b';
 use Mojo::Util qw/secure_compare hmac_sha1_sum/;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 # Todo:
 # - Make everything async (top priority)
@@ -1314,11 +1314,7 @@ and needs to be accessible from the web.
 
   $ perl examples/pubsubapp daemon
 
-=begin html
-
-<div style="text-align: center;"><img src="http://sojolicio.us/images/pubsubhubbub-screenshot.png" alt="PubSubHubbub Example Application" /></div>
-
-=end html
+=for html <br /><div style="text-align: center;"><img src="http://sojolicio.us/images/pubsubhubbub-screenshot.png" alt="PubSubHubbub Example Application" /></div>
 
 This example may be a good starting point for your own implementation, especially,
 if you deal with the subscriber part.
